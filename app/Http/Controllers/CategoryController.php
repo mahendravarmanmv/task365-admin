d43120430f322733a0f057d9b3d8be3a89ae9069
Mahendra varma <mahendra.varma22@gmail.com>
2025-06-20 00:22:31 +0530

    fixes.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::latest()->paginate(10);
+        $categories = Category::latest()->get();
         return view('categories.index', compact('categories'));
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
 
 <body class="app sidebar-mini">
   <!-- Navbar-->
-  <header class="app-header"><a class="app-header__logo" href="index.html">Task-365</a>
+  <header class="app-header"><a class="app-header__logo" href="{{ route('dashboard') }}">Task-365</a>
     <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
     <!-- Navbar Right Menu-->
     <ul class="app-nav">
